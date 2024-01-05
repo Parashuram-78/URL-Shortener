bfbf116f5a73020f96f0a73cdc3d964f24ef5295
Ankur Shukla <itzankurhere@gmail.com>
2024-01-05 18:42:23 +0530

    DB Up and Running

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 const dbConnect=()=>{
-mongoose.connect('mongodb://127.0.0.1:27017',{
+mongoose.connect("mongodb+srv://ankurshuklamet19:AhgL2uJtIzV31AKd@cluster0.yjr8v49.mongodb.net/?retryWrites=true&w=majority",{
     dbname:'URLShortener',
 }).then(()=>{
     console.log("Database connected")
